3658f24594237533786202c6175b0e74b5c569ce
podumai <kirillsm05@gmail.com>
2025-11-11 02:21:57 +0700

    Changed conan recipe to use ninja.

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,23 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 class Containers(ConanFile):
     settings: tuple[str] = ("os", "compiler", "build_type", "arch")
-    generators: tuple[str] = ("CMakeToolchain", "CMakeDeps")
+    generators: tuple[str] = ("CMakeDeps",)
 
     def requirements(self) -> None:
         self.requires("catch2/3.11.0")
+
+    def generate(self) -> None:
+        tc = CMakeToolchain(self)
+        tc.generator = "Ninja"
+        tc.variables["CMAKE_CXX_STANDARD"] = "23"
+        tc.generate()
+
+    def build(self) -> None:
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def layout(self) -> None:
         cmake_layout(self)

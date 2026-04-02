366d55e02ce59e28e6b65d9ed2772bb17f9b9825
podumai <kirillsm05@gmail.com>
2026-04-02 13:38:20 +0700

    refactor: replace macro with function

--- include/lab/containers/assert.hpp
+++ include/lab/containers/assert.hpp
@@ -1,17 +1,18 @@
 #pragma once
 
 #include <cassert>
+#include <string_view>
 #include <iostream>
 
-#ifndef NDEBUG
-  #define LAB_CONTAINERS_ASSERT(condition, message) \
-    do {                                            \
-      if (!(condition)) {                           \
-        std::cerr << (message) << '\n';             \
-      }                                             \
-      assert((condition));                          \
-    } while (false)
-#else
-  #define LAB_CONTAINERS_ASSERT(condition, message) \
-    (void) int { }
-#endif
+namespace lab::utility {
+
+inline auto LabContainersAssert(bool condition, std::string_view message) -> void {
+  #ifndef NDEBUG
+  if (!condition) {
+    std::cerr << message << '\n';
+    assert(false);
+  }
+  #endif
+}
+
+}
